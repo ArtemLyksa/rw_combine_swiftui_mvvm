f425d186bb8e39a427b80ba0ed26b648a945db61
Artem <artemlyksa@gmail.com>
2019-12-11 17:08:51 +0200

    Added fetching and displaying city weather using swiftUi and Combine

--- CombinedWeatherApp/Weather/WeeklyWeather/WeeklyWeatherViewModel.swift
+++ CombinedWeatherApp/Weather/WeeklyWeather/WeeklyWeatherViewModel.swift
@@ -26,3 +26,58 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import SwiftUI
+import Combine
+
+class WeeklyWeatherViewModel: ObservableObject, Identifiable {
+  
+  @Published var city: String = ""
+  @Published var dataSource: [DailyWeatherRowViewModel] = []
+
+  private let weatherFetcher: WeatherFetchable
+
+  private var disposables = Set<AnyCancellable>()
+
+  init(weatherFetcher: WeatherFetchable,
+       scheduler: DispatchQueue = DispatchQueue(label: "WeatherViewModel")) {
+    
+    self.weatherFetcher = weatherFetcher
+    _ = $city
+      .dropFirst()
+      .debounce(for:
+      .seconds(0.5), scheduler: scheduler)
+      .sink(receiveValue: fetchWeather(forCity:))
+  }
+  
+  func fetchWeather(forCity city: String) {
+    // 1
+    weatherFetcher.weeklyWeatherForecast(forCity: city)
+      .map { response in
+        response.list.map(DailyWeatherRowViewModel.init)
+      }
+
+      .map(Array.removeDuplicates)
+      .receive(on: DispatchQueue.main)
+      .sink(
+        receiveCompletion: { [weak self] value in
+          guard let self = self else { return }
+          switch value {
+          case .failure:
+            self.dataSource = []
+          case .finished:
+            break
+          }
+        },
+        receiveValue: { [weak self] forecast in
+          guard let self = self else { return }
+          self.dataSource = forecast
+      })
+      .store(in: &disposables)
+  }
+}
+
+extension WeeklyWeatherViewModel {
+  var currentWeatherView: some View {
+    return WeeklyWeatherBuilder.makeCurrentWeatherView(withCity: city, weatherFetcher: weatherFetcher)
+  }
+}
